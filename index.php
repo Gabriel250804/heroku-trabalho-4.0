13c3e0585fd5f4c969010543d0893a2a90f62486
GabrielHaab <haabgabriel@gmail.com>
2022-01-27 20:31:18 -0300

    make it better

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
 </head>
 <body>
     <div class="mainContent">
-        <img class="middlePhoto1" src="Img/cs-go-2_BIeFUjZ.jpg" width="10%" height="10%">
-        <img class="middlePhoto2" src="Img/Praias-do-Sul-Praia-do-Estaleiro.jpg" width="10%" height="10%">
-        <img  class="middlePhoto3" src="Img/MAN-UTD.jpg" width="10%" height="10%">
-        <img  class="middlePhoto4" src="Img/49434434858_10a44622e1_o.jpg" width="10%" height="10%">
-        <img  class="middlePhoto5" src="Img/avião_011.jpg" width="10%" height="10%">	
+        <img class="middlePhoto1" src="cs-go-2_BIeFUjZ.jpg" width="10%" height="10%">
+        <img class="middlePhoto2" src="Praias-do-Sul-Praia-do-Estaleiro.jpg" width="10%" height="10%">
+        <img  class="middlePhoto3" src="MAN-UTD.jpg" width="10%" height="10%">
+        <img  class="middlePhoto4" src="49434434858_10a44622e1_o.jpg" width="10%" height="10%">
+        <img  class="middlePhoto5" src="avião_011.jpg" width="10%" height="10%">	
     </div>
     <h2>Neste site feito por mim, Gabriel Haab,  estão expostas algumas imagens de coisas e atividades que eu gosto. Como por exemplo jogar Cs-go, praias, futebol, basquete e viajar</h2>
 </body>
