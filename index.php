60e4aa25b0b2b13ee6136e009300687fade7c619
GabrielHaab <haabgabriel@gmail.com>
2022-01-27 20:29:02 -0300

    make it better

--- index.php
+++ index.php
@@ -5,19 +5,22 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="en">
 <head>
- <meta charset="UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <title>Heroku_4.0</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>HaabLife</title>
 </head>
-
 <body>
- <img src="cabo.jfif">
- <img src="lixo.jfif">
- <img src="serie.jfif">
- <h1>Aula Heroku_4.0</h1>
-</body> 
-
+    <div class="mainContent">
+        <img class="middlePhoto1" src="Img/cs-go-2_BIeFUjZ.jpg" width="10%" height="10%">
+        <img class="middlePhoto2" src="Img/Praias-do-Sul-Praia-do-Estaleiro.jpg" width="10%" height="10%">
+        <img  class="middlePhoto3" src="Img/MAN-UTD.jpg" width="10%" height="10%">
+        <img  class="middlePhoto4" src="Img/49434434858_10a44622e1_o.jpg" width="10%" height="10%">
+        <img  class="middlePhoto5" src="Img/avião_011.jpg" width="10%" height="10%">	
+    </div>
+    <h2>Neste site feito por mim, Gabriel Haab,  estão expostas algumas imagens de coisas e atividades que eu gosto. Como por exemplo jogar Cs-go, praias, futebol, basquete e viajar</h2>
+</body>
 </html>
